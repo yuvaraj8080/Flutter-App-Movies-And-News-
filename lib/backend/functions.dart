b313e8cc40303760f70398276d8959c49e73bad9
yuvaraj8080 <yuvarajdekhane8080@gmail.com>
2023-12-16 16:05:39 +0530

    shere on github

---
+++ lib/backend/functions.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:technewz/components/searchbar.dart';
+import 'package:technewz/utils/key.dart';
+
+Future<List> fetchnews() async {
+  final response = await http.get(
+    Uri.parse('https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=100&apiKey=' +
+        Key.key +
+        '&q=' +
+        SearchBar.searchcontroller.text),
+  );
+  Map result = jsonDecode(response.body);
+  print('Fetched');
+
+  return (result['articles']);
+}
