593d0f41db009c22d73535ef68e597b922a01bd2
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2023-12-26 20:02:27 +0530

    Delete lib/utils directory

--- lib/utils/constants.dart
+++
@@ -1,4 +0,0 @@
-class Constants {
-  static String imageurl =
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGkAznCVTAALTD1o2mAnGLudN9r-bY6klRFB35J2hY7gvR9vDO3bPY_6gaOrfV0IHEIUo&usqp=CAU';
-}
